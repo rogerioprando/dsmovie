e28277b78eaafb5896736423f17b018cbe143511
Rogerio Prando <rogerio.prando@wplex.com.br>
2022-01-10 18:03:22 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
-import Navbar from "./components/Navbar";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
 
-
-// Aqui no componente App é chamado o componente Navbar.
+// Aqui no componente App são chamados os componentes do projeto.
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter> 
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
