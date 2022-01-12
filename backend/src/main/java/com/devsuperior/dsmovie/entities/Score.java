45ecabd25a0cc48323fb6844720d4c48b6021d89
Rogerio Prando <rogerio.prando@wplex.com.br>
2022-01-12 11:34:57 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,47 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+	
+	@EmbeddedId
+	
+	private ScorePk id = new ScorePk();
+	private Double value;
+	
+	public Score() {
+		
+	}
+	
+	// quem tem a referência do movie é a classe ScorePk
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+	
+	public ScorePk getId() {
+		return id;
+	}
+
+	public void setId(ScorePk id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+	
+
+}
